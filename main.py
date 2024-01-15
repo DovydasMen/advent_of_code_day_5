4a209967a0bdf0678c9852506431943edc61ef99
Dovydas <dovydas.menkevicius@qdevtechnologies.com>
2024-01-15 16:07:09 +0200

    init

--- main.py
+++ main.py
@@ -1,55 +1,52 @@
-from typing import List, Tuple
+from typing import List, Generator
+from pathlib import Path
+from pprint import pprint
 
 
-def read_lines_from_file(file_path) -> List[str]:
-    """Read lines from a file, trims whitespace and ignores empty lines."""
-    with open(file_path, "r") as file:
-        return [line.strip() for line in file if line.strip()]
+def get_seeds(lines: List[str]) -> Generator:
+    """(int(value) for value in full_almanac[0].split() if value.isdigit())
+    Same as below in my case readability is better with function
+    """
+
+    line_values = lines[0].split()
+    for value in line_values:
+        if value.isdigit():
+            yield int(value)
 
 
-def get_seeds(lines: list) -> list:
-    return list(map(int, lines[0].strip("seeds: ").split()))
-
-
-def get_almanac_without_seeds(full_almanac: List[str]) -> Tuple[List[int], List[str]]:
-    seeds = get_seeds(full_almanac)
-    del full_almanac[0]
-    return seeds, full_almanac
-
-
-def get_mapping_map(map: List[str]) -> List[List[int]]:
-    mapping_map = [[]]
-    mapping_place = -1
-
-    for value in map:
-        if value.endswith(":"):
-            mapping_place += 1
-            mapping_map.append([])
+def get_alignment_map(almanac: List[str]) -> Generator:
+    ally = []
+    counter = -1
+    for value in almanac[1:]:
+        if value == "":
             continue
+        elif value.endswith(":"):
+            ally.append([])
+            counter += 1
         else:
-            mapping_map[mapping_place].append([int(number) for number in value.split()])
+            ally[counter].append([int(val) for val in value.split()])
 
-    return mapping_map
+    for aligment in ally:
+        yield aligment
 
 
-def get_locations(seeds: List[int], mapping_map: List[List[int]]) -> List[int]:
+def get_locations(seeds: Generator, alignment_map: Generator) -> List[int]:
     locations = []
     for seed in seeds:
         location = seed
-        for mapping in mapping_map:
-            for details in mapping:
-                if location in range(details[1], details[1] + details[2]):
-                    destination, source, _ = details
-                    diffrence = location - source
-                    location = destination + diffrence
+        for ally_map in alignment_map:
+            for value in ally_map:
+                destination, source, symbols_range = value
+                if location in range(int(source), int(source) + int(symbols_range)):
+                    diffrence = location - int(source)
+                    location = int(destination) + int(diffrence)
                     break
         locations.append(location)
-
     return locations
 
 
 if __name__ == "__main__":
-    full_almanac = read_lines_from_file("input.txt")
-    seeds, almanac_without_seeds = get_almanac_without_seeds(full_almanac)
-    mapping_map = get_mapping_map(almanac_without_seeds)
-    print(min(get_locations(mapping_map=mapping_map, seeds=seeds)))
+    full_almanac = Path("input.txt").read_text().splitlines()
+    seeds = get_seeds(full_almanac)
+    alignment_map = get_alignment_map(full_almanac)
+    print(min(get_locations(seeds=seeds, alignment_map=alignment_map)))
