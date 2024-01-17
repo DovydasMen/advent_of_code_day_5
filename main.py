760721193a326e26c5f5dd175a49a774a1a54602
Dovydas <dovydas.menkevicius@qdevtechnologies.com>
2024-01-17 10:21:37 +0200

    Fixes generators caused issues

--- main.py
+++ main.py
@@ -15,26 +15,29 @@ def get_seeds(lines: List[str]) -> Generator:
 
 
 def get_alignment_map(almanac: List[str]) -> Generator:
-    ally = []
+    alignment_map = []
     counter = -1
     for value in almanac[1:]:
         if value == "":
             continue
         elif value.endswith(":"):
-            ally.append([])
+            alignment_map.append([])
             counter += 1
         else:
-            ally[counter].append([int(val) for val in value.split()])
+            alignment_map[counter].append([int(val) for val in value.split()])
 
-    for aligment in ally:
-        yield aligment
+    for alignment in alignment_map:
+        yield alignment
 
 
-def get_locations(seeds: Generator, alignment_map: Generator) -> List[int]:
+def get_locations(seeds: Generator, almanac: List[str]) -> List[int]:
     locations = []
     for seed in seeds:
+        alignment_map = get_alignment_map(almanac=almanac)  # Generatorių reikia konstruoti kiekvieną kart iš naujo.
+        #Ex solution buvo pacio generatoriuas padavimas į funkciją. Bėda, kad generatorius negrįžo į praždią.
         location = seed
         for ally_map in alignment_map:
+            pprint(ally_map)
             for value in ally_map:
                 destination, source, symbols_range = value
                 if location in range(int(source), int(source) + int(symbols_range)):
@@ -48,5 +51,4 @@ def get_locations(seeds: Generator, alignment_map: Generator) -> List[int]:
 if __name__ == "__main__":
     full_almanac = Path("input.txt").read_text().splitlines()
     seeds = get_seeds(full_almanac)
-    alignment_map = get_alignment_map(full_almanac)
-    print(min(get_locations(seeds=seeds, alignment_map=alignment_map)))
+    print(min(get_locations(seeds=seeds, almanac=full_almanac)))
